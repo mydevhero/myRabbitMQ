a8b7797662ff720cfdca6b4e800f201846b6a530
Alessandro Bianco <mydevhero@gmail.com>
2024-01-08 12:09:33 +0100

    Configured "build" script

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  mode: "production",
+  target: 'node',
+  entry: './src/myRabbitMQ.ts', // Il punto di ingresso del tuo modulo
+  output: {
+    filename: 'bundle.js', // Il nome del file di output
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    plugins: [new TsconfigPathsPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
